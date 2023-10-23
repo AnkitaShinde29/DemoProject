12e535e7d07dcd838ad1439b2d7d048524983b94
AnkitaShinde29 <ankita.shinde2905@gmail.com>
2023-10-23 23:56:46 +0530

    Print

---
+++ DemoTest/src/com/testgit/PrintedNumbers.java
@@ -0,0 +1,13 @@
+package com.testgit;
+
+public class PrintedNumbers {
+
+	public static void main(String[] args) {
+		for(int i=0; i<=10; i++) {
+			
+		   System.out.println("Printed numbers"+i);
+		}
+
+	}
+
+}
